593d527c73000afad5bf0ef5297fe634e8b848cc
Cuco <Cuco@gmail.com>
2020-01-04 15:52:58 -0300

    Commit code challenge to development branch. TmdbApi: 1. Moved API_KEY to BuildConfig 2. Added searchUpcomingMovies to filter movies.
    BaseActivity:
    Deleted api initialization with DI.
    
    MoviesRemoteDataSource:
    Added to manage Retrofit data source.
    
    DefaultMovieRepository:
    Added to manage diferent ways to access data sources.(MoviesRemoteDataSource so far, but if the project grows there could be other datasources)
    
    Details:
    DetailsActivity: Detail view.
    DetailsViewModel: To manage DetailsActivity.
    GenresAdapter: RecyclerView adapter for Movie's genres.
    GenresListBinding: binding adapter submitList to view.
    
    di(Dependency Injection using Koin):
    appModule: Main dependencies.
    networkModule: Network dependencies.
    
    home:
    HomeActivity:
    Added search.
    Moved logic to HomeViewModel.
    Added EndlessRecycler... for pagination. I would like to use Jetpack paging but I had some dependencies issues.
    HomeAdapter:
    Adapter to the Movies recylerview.
    HomeViewModel: To manage HomeActivity and HomeAdapter.
    MoviesListBinding: Binding from adapter to view.
    
    Models:
    Changed @Json(name ="...") to @SerializedName("")
    
    SplashActivity:
    Deleted and the genres logic was moved to HomeActivity.
    
    utils:
    EndlessRecyclerOnScrollListener: Scroller for pagination.
    
    Event:
    To handle livedata object.
    
    activity_detail: UI to movies details.
    genre_item: Card to genres recyclerview.
    movie_item: Updated to use databinding.
    splash_activity: Deleted
    
    menu_home: To add search option.
    
    Edited other xml files(dimens, string, style...).
    
    Updated AndroidManifest.
    
    Added few testes to viewmodels
    
    build.gradle(Main)
    Updated kotlin version.
    Updated build gradle plugin to 3.5.2
    Added versions var.
    
    build.gradle(app):
    apply plugin: 'kotlin-kapt'.
    Updated compileSdkVersion and targetSdkVersion to 28.
    Migrated to androidx.
    Added API_KEY.
    Added databinding support.
    Updated dependencies.
    
    gradle.propesties:
    Added API_KEY
    
    I would like to use fragments navigation instead of Activities, but in that architectural decision now I only offer my point of view.

---
+++ app/src/main/java/com/arctouch/codechallenge/details/GenresAdapter.kt
@@ -0,0 +1,50 @@
+package com.arctouch.codechallenge.details
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.arctouch.codechallenge.databinding.GenreItemBinding
+
+class GenresAdapter() :
+        ListAdapter<String, GenresAdapter.ViewHolder>(IssueDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+
+        holder.bind(item)
+    }
+
+    class ViewHolder private constructor(val binding: GenreItemBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: String) {
+
+            binding.titleGenre.text = item
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = GenreItemBinding.inflate(layoutInflater, parent, false)
+
+                return ViewHolder(binding)
+            }
+        }
+    }
+}
+
+class IssueDiffCallback : DiffUtil.ItemCallback<String>() {
+    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+}
---
+++ app/src/test/java/com/arctouch/codechallenge/details/DetailsViewModelTest.kt
@@ -0,0 +1,96 @@
+package com.arctouch.codechallenge.details
+
+import android.app.Application
+import com.arctouch.codechallenge.BuildConfig
+import com.arctouch.codechallenge.api.TmdbApi
+import com.arctouch.codechallenge.data.sources.DefaultMovieRepository
+import com.arctouch.codechallenge.di.appModule
+import com.arctouch.codechallenge.di.networkModule
+import com.arctouch.codechallenge.model.Genre
+import com.arctouch.codechallenge.model.Movie
+import io.reactivex.Observable
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.hasProperty
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.ExpectedException
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.core.inject
+import org.koin.test.AutoCloseKoinTest
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import retrofit2.HttpException
+
+
+class DetailsViewModelTest : AutoCloseKoinTest() {
+
+    private lateinit var mockedGenres: ArrayList<Genre>
+
+    private lateinit var movie: Movie
+
+    @Before
+    fun setUptest() {
+        val modulesList = listOf(networkModule, appModule)
+        startKoin {
+            modules(modulesList)
+        }
+
+        movie = Movie(4, "Film", "OverView", emptyList(), emptyList(), "/poster.png", "/backdrop.png", "2020-05-05")
+    }
+
+    @Test
+    fun startWithApi() {
+        val defaultMovieRepository: DefaultMovieRepository by inject()
+        val test = defaultMovieRepository.getMovie(419704, BuildConfig.API_KEY, TmdbApi.DEFAULT_LANGUAGE).test()
+
+        test.awaitTerminalEvent()
+
+        test.assertNoErrors()
+    }
+
+    @Test
+    fun startWithWrongApi() {
+        val defaultMovieRepository: DefaultMovieRepository by inject()
+        val test = defaultMovieRepository.getMovie(419704, "sdlkfsldkf", TmdbApi.DEFAULT_LANGUAGE).test()
+
+        test.awaitTerminalEvent()
+
+        test.assertError(HttpException::class.java)
+    }
+
+    @Test
+    fun startWithWrongId() {
+        val defaultMovieRepository: DefaultMovieRepository by inject()
+        val test = defaultMovieRepository.getMovie(1, BuildConfig.API_KEY, TmdbApi.DEFAULT_LANGUAGE).test()
+
+        test.awaitTerminalEvent()
+
+        test.assertError(HttpException::class.java)
+    }
+
+    @Test
+    fun startWithData() {
+
+        val localDefaultMovieRepository = mock(DefaultMovieRepository::class.java)
+
+        val observable = Observable.just(movie)
+
+        `when`(localDefaultMovieRepository.getMovie(1, BuildConfig.API_KEY, TmdbApi.DEFAULT_LANGUAGE)).thenReturn(observable)
+
+        val test = localDefaultMovieRepository.getMovie(1, BuildConfig.API_KEY, TmdbApi.DEFAULT_LANGUAGE).test()
+
+        test.awaitTerminalEvent()
+
+        test.assertValue(movie)
+    }
+
+    @After
+    fun finish() {
+        stopKoin()
+    }
+
+}
